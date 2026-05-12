602115de2caff03920c6a44e82a8ea1c7acdcbc1
khalid0931 <mutiarif53@gmail.com>
2026-05-12 10:30:46 +0700

    membuat public node

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -5,3 +5,12 @@ using namespace std;
 //node class representating a single node in the linked list
 class Node
 {
+    public:
+    int data;
+    Node *next;
+
+    Node()
+    {
+        next = NULL;
+    }
+};
